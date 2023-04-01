221f68e4f1884353b07a4ddd2934beef8b99e008
Maral Bahari <maralbahari75@yahoo.com>
2023-04-02 01:31:24 +0800

    fix logger display

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import argparse
 import logging
+import sys
 
 from click import prompt
 
 from train.train_pointnet import train
 from test.test_pointnet import test
 
-def run_train():
-    logging.log(level=1,msg='=================Training begins====================')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+
+logger.addHandler(ch)
+
+def run_train(config_uri: str):
+    logger.info(msg='=================Training begins====================')
 
     train(config_uri)
 
-def run_test():
-    logging.log(level=1,msg='=================Testing begins====================')
+def run_test(config_uri: str):
+    logger.info(msg='=================Testing begins====================')
     pass
 
 if __name__ == "__main__":
@@ -25,14 +34,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
     config_uri: str = args.config
 
-    print('Please choose the option that you wish to run:')
+    logger.info('Please choose the option that you wish to run:')
 
     option: int = prompt('1. Train a dataset on PointNet\n2. Test your model\nOption', value_proc=int)
     print()
 
     if option == 1:
-        train(config_uri)
+        run_train(config_uri)
     elif option == 2:
-        test(config_uri)
+        run_test(config_uri)
     else:
         raise ValueError('Provided wrong input')
